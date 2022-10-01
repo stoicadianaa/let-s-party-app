cfa51050acb764a29ed02aaa4b92852eff4d1798
Diana Stoica <diana-c.stoica@outlook.com>
2022-10-02 00:09:20 +0300

    Created screen logic

---
+++ lib/app/login/login_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lets_party/core/service/firebase_service.dart';
+import 'package:provider/provider.dart';
+
+class LoginBloc extends ChangeNotifier {
+  final FirebaseService _firebaseService = FirebaseService();
+  bool visiblePassword = false;
+
+  Future<void> login (BuildContext context, String? email, String? password) async {
+    final String? errorMessage =
+    await _firebaseService.mailSignIn(email, password);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(errorMessage ?? "Authentication successful"),
+      ),
+    );
+  }
+
+  void changePasswordVisibility() => visiblePassword = !visiblePassword;
+}
