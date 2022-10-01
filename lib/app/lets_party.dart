4a8ffbc98ae757a21246a80a00177ef3682b619c
Diana Stoica <diana-c.stoica@outlook.com>
2022-10-01 16:55:01 +0300

    Added responsive_framework package

--- lib/app/lets_party.dart
+++ lib/app/lets_party.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lets_party/app/home/home_screen.dart';
 import 'package:lets_party/constants/app_colors.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,6 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: appThemeData,
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: const [
+          ResponsiveBreakpoint.resize(480, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
