63800dac9dbb5010c702420b5c8b051458220830
Steven Billich <stevenbillich17@gmail.com>
2022-10-01 18:54:36 +0300

    added home menu and home appbar

--- lib/app/home/home_screen.dart
+++ lib/app/home/home_screen.dart
@@ -1,86 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lets_party/constants/app_colors.dart';
+import 'package:lets_party/constants/app_dimens.dart';
+import 'package:lets_party/gen/fonts.gen.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0.0,
+          leadingWidth: 100.0,
+          toolbarHeight: 80.0,
+          leading: TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Back",
+              style: TextStyle(
+                color: gamboge,
+                fontSize: 20.0,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          title: const Text(
+            "let's party!",
+            style: TextStyle(
+              color: jet,
+              fontSize: 42.0,
+              fontFamily: FontFamily.keepOnTrucking,
             ),
-          ],
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: AppDimens.padding_2x, vertical: AppDimens.padding_2x),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                HomeMenu(),
+              ],
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomeMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        PageButton(onPressed: (){}, svgAssetPath: "assets/images/create_party.svg" , text: "create party"),
+        PageButton(onPressed: (){}, svgAssetPath: "assets/images/my-invites.svg" , text: "my invites"),
+        PageButton(onPressed: (){}, svgAssetPath: "assets/images/party-games.svg" , text: "party games"),
+        PageButton(onPressed: (){}, svgAssetPath: "assets/images/settings.svg" , text: "settings"),
+      ],
+    );
+  }
+}
+
+class PageButton extends StatelessWidget {
+  late VoidCallback onPressed;
+  late String svgAssetPath;
+  late String text;
+
+  PageButton({required this.onPressed, required this.svgAssetPath, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: onPressed,
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all(const CircleBorder()),
+              shadowColor: MaterialStateProperty.all(Colors.grey),
+              backgroundColor: MaterialStateProperty.all(gamboge)),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: SvgPicture.asset(
+              svgAssetPath,
+              width: 30.0,
+              height: 30.0,
+            ),
+          ), //shape: CircleBorder(),
+        ),
+        Text(
+          text,
+          style: const TextStyle(
+            fontSize: 12.5,
+          ),
+        )
+      ],
     );
   }
 }
