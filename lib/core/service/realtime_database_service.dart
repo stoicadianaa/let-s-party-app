c494854b752320380ad41500c3a75e26eef22906
Diana Stoica <diana-c.stoica@outlook.com>
2022-10-03 16:20:52 +0300

    Created database and classes for categories and items

--- lib/core/service/realtime_database_service.dart
+++ lib/core/service/realtime_database_service.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:lets_party/core/model/categories_model.dart';
 import 'package:lets_party/core/model/user_model.dart';
 
 class RealtimeDatabaseService {
+  List<CategoriesModel> listOfCategories = [];
+  bool loadingDone = false;
+  RealtimeDatabaseService();
+
   Future<String?> setUserInfo(UserModel user) async {
-    final DatabaseReference usersRef =
-        FirebaseDatabase.instance.ref("users/${user.email!.replaceAll(".", "")}");
+    final DatabaseReference usersRef = FirebaseDatabase.instance
+        .ref("users/${user.email!.replaceAll(".", "")}");
     try {
       if (user.birthday == null) return "Empty date";
       if (user.name == null) return "Empty name";
@@ -13,5 +18,18 @@ class RealtimeDatabaseService {
     } catch (ex) {
       return ex.toString();
     }
+  }
+
+  Future<void> getCategories() async {
+    final ref = FirebaseDatabase.instance.ref("categories/");
+    final event = await ref.once();
+    final Map<String, dynamic> categories =
+        Map<String, dynamic>.from(event.snapshot.value as Map);
+    for (final category in categories["listOfCategories"] as List) {
+      listOfCategories.add(
+        CategoriesModel.fromMap(category as Map),
+      );
+    }
+    loadingDone = true;
   }
 }
