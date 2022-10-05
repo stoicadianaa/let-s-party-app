1f37a9e7ee11e3c7f3fea4790150754066064e6d
Steven Billich <stevenbillich17@gmail.com>
2022-10-05 19:58:55 +0300

    refactor home screen to use different components

--- lib/app/home/home_screen.dart
+++ lib/app/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lets_party/app/home/components/home_menu.dart';
+import 'package:lets_party/app/home/components/party_button.dart';
 import 'package:lets_party/constants/app_colors.dart';
 import 'package:lets_party/constants/app_dimens.dart';
 import 'package:lets_party/gen/fonts.gen.dart';
@@ -121,122 +122,6 @@ class MyHomePage extends StatelessWidget {
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class HomeMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        PageButton(
-            onPressed: () {},
-            svgAssetPath: "assets/images/create_party.svg",
-            text: "create party"),
-        PageButton(
-            onPressed: () {},
-            svgAssetPath: "assets/images/my-invites.svg",
-            text: "my invites"),
-        PageButton(
-            onPressed: () {},
-            svgAssetPath: "assets/images/party-games.svg",
-            text: "party games"),
-        PageButton(
-            onPressed: () {},
-            svgAssetPath: "assets/images/settings.svg",
-            text: "settings"),
-      ],
-    );
-  }
-}
-
-class PageButton extends StatelessWidget {
-  late VoidCallback onPressed;
-  late String svgAssetPath;
-  late String text;
-
-  PageButton(
-      {required this.onPressed,
-      required this.svgAssetPath,
-      required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: onPressed,
-          style: ButtonStyle(
-              shape: MaterialStateProperty.all(const CircleBorder()),
-              shadowColor: MaterialStateProperty.all(Colors.grey),
-              backgroundColor: MaterialStateProperty.all(gamboge)),
-          child: Padding(
-            padding: const EdgeInsets.all(AppDimens.padding_2x),
-            child: SvgPicture.asset(
-              svgAssetPath,
-              width: 30.0,
-              height: 30.0,
-            ),
-          ), //shape: CircleBorder(),
-        ),
-        Text(
-          text,
-          style: const TextStyle(
-            fontSize: 12.5,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class PartyButton extends StatelessWidget {
-  String image;
-  String name;
-  VoidCallback onPressed;
-
-  PartyButton(
-      {required this.image, required this.name, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: AppDimens.padding),
-      child: RawMaterialButton(
-        onPressed: onPressed,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 130,
-                      child: Image.asset(
-                        image,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(AppDimens.padding),
-              child: Text(
-                name,
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            )
-          ],
         ),
       ),
     );
