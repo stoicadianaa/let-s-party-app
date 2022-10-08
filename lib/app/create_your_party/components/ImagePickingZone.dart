6fad234fa0c49697dc36fab05047f39e46424532
Steven Billich <stevenbillich17@gmail.com>
2022-10-08 20:10:59 +0300

    refactor image picking zone

--- lib/app/create_your_party/components/ImagePickingZone.dart
+++ lib/app/create_your_party/components/ImagePickingZone.dart
@@ -8,14 +8,11 @@ import 'package:lets_party/constants/app_dimens.dart';
 class ImagePickingZone extends StatefulWidget {
   const ImagePickingZone({Key? key}) : super(key: key);
 
-
-
   @override
   _ImagePickingZoneState createState() => _ImagePickingZoneState();
 }
 
 class _ImagePickingZoneState extends State<ImagePickingZone> {
-
   File? image;
   bool? isImagedPicked = false;
 
@@ -23,13 +20,13 @@ class _ImagePickingZoneState extends State<ImagePickingZone> {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
-      if(image == null) return;
+      if (image == null) return;
 
       final imageTemp = File(image.path);
 
       setState(() => this.image = imageTemp);
       this.isImagedPicked = true;
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
   }
@@ -39,44 +36,57 @@ class _ImagePickingZoneState extends State<ImagePickingZone> {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.camera);
 
-      if(image == null) return;
+      if (image == null) return;
 
       final imageTemp = File(image.path);
 
       setState(() => this.image = imageTemp);
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: AppDimens.padding_2x),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height*0.25,
-        width: MediaQuery.of(context).size.width,
-        child: TextButton(
-          onPressed: () {
-            pickImage();
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.grey.shade400),
-            shape:  MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                )
-            ),
-          ),
-
-          child: Text(
-            "add photo",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.grey.shade700,
-            ),
-          ),
+      padding:
+          EdgeInsets.symmetric(vertical: 0.0, horizontal: AppDimens.padding_2x),
+      child: GestureDetector(
+        onTap: () {
+          pickImage();
+        },
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+                image: image != null
+                    ? DecorationImage(
+                        image: FileImage(image!),
+                        fit: BoxFit.fill,
+                      )
+                    : null,
+                borderRadius: BorderRadius.circular(18.0),
+              ),
+              child: image == null
+                  ? Center(
+                      child: Text(
+                        "add photo",
+                        style: TextStyle(
+                            fontSize: 25, color: Colors.grey.shade800),
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: AppDimens.padding,
+                          horizontal: AppDimens.padding),
+                      child: Text(
+                        "change photo",
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.grey.shade900),
+                      ),
+                    )),
         ),
       ),
     );
