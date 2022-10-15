39996e23099cf5d7a296033139494b2ca013fd77
Diana Stoica <diana-c.stoica@outlook.com>
2022-10-15 17:02:52 +0300

    Added the changing profile picture functionality

--- lib/app/edit_profile/edit_profile_bloc.dart
+++ lib/app/edit_profile/edit_profile_bloc.dart
@@ -1,21 +1,62 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:lets_party/core/model/user_model.dart';
-import 'package:lets_party/core/service/firebase_auth_service.dart';
 import 'package:lets_party/core/service/realtime_database_service.dart';
 
-class EditProfileBloc extends ChangeNotifier{
-  UserModel user;
-  String profilePicture;
-  RealtimeDatabaseService _service = RealtimeDatabaseService();
+class EditProfileBloc extends ChangeNotifier {
+  late UserModel user;
+  late String profilePicture;
+  final RealtimeDatabaseService _service = RealtimeDatabaseService();
+  bool isLoadingDone = false;
+  bool visiblePassword = false;
+  bool? isImagedPicked = false;
+  File? image;
 
   EditProfileBloc() {
     loadScreen();
-
   }
 
-  loadScreen () async {
-    user = await _service.getUserFromFirebase(FirebaseAuth.instance.currentUser!.email!);
+  //duplicate, move at mixins
+  Future<void> pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+      this.image = imageTemp;
+      isImagedPicked = true;
+      await _service.setProfileImage(user.email!, this.image!);
+
+      profilePicture = await RealtimeDatabaseService.getProfileImage(user.email!);
+
+      notifyListeners();
+
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
   }
 
+  Future<void> loadScreen() async {
+    final String email = FirebaseAuth.instance.currentUser!.email!;
+    user = await _service.getUserFromFirebase(email);
+    profilePicture = await RealtimeDatabaseService.getProfileImage(email);
+    isLoadingDone = true;
+    notifyListeners();
+  }
+
+  static Future<void> changeUserNameAndPassword(String newPassword, String newName) async {
+    final RealtimeDatabaseService _service = RealtimeDatabaseService();
+    var user = FirebaseAuth.instance.currentUser;
+    try {
+      user!.updatePassword(newPassword);
+      _service.updateUserName(newName, user.email!);
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
--- lib/app/edit_profile/edit_profile_screen.dart
+++ lib/app/edit_profile/edit_profile_screen.dart
@@ -1,133 +1,175 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lets_party/app/components/input_field_widget.dart';
+import 'package:lets_party/app/edit_profile/edit_profile_bloc.dart';
 import 'package:lets_party/constants/app_colors.dart';
 import 'package:lets_party/constants/app_dimens.dart';
 import 'package:lets_party/constants/app_styles.dart';
+import 'package:provider/provider.dart';
 
 class EditProfileScreen extends StatelessWidget {
   EditProfileScreen({super.key});
 
+  final _editProfileKey = GlobalKey<FormState>();
+  String? _password1;
   String? _name;
 
   @override
   Widget build(BuildContext context) {
-    const imageLink =
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
     final screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "profile",
-          style: AppStyles.appBarStyle.copyWith(color: babyPowder),
+
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => EditProfileBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "profile",
+            style: AppStyles.appBarStyle.copyWith(color: babyPowder),
+          ),
         ),
-      ),
-      body: SafeArea(
-        child: ListView(
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  color: gamboge,
-                  height: 200.0,
-                ),
-                Positioned(
-                  top: 100.0,
-                  left: screenWidth / 4,
-                  child: Container(
-                    height: screenWidth / 2,
-                    width: screenWidth / 2,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(imageLink),
+        body: SafeArea(
+          child: Consumer<EditProfileBloc>(
+            builder: (context, bloc, child) {
+              if (bloc.isLoadingDone) {
+                return ListView(
+                  children: [
+                    Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Container(
+                          color: gamboge,
+                          height: 200.0,
+                        ),
+                        Positioned(
+                          top: 100.0,
+                          left: screenWidth / 4,
+                          child: Container(
+                            height: screenWidth / 2,
+                            width: screenWidth / 2,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(bloc.profilePicture),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: screenWidth / 4 + 50.0,
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        await bloc.pickImage();
+                      },
+                      child: Text(
+                        "Change your profile picture",
+                        style: AppStyles.bodyStyle
+                            .copyWith(color: Colors.grey[300]),
                       ),
                     ),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: screenWidth / 4 + 50.0,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "Change your profile picture",
-                style: AppStyles.bodyStyle.copyWith(color: Colors.grey[300]),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(AppDimens.padding_2x),
-              child: Column(
-                children: [
-                  InputField(
-                    hintText: "Name",
-                    onChanged: (val) {
-                      _name = val;
-                    },
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (email) {
-                      if (email != null && email != '') {
-                        return null;
-                      }
-                      return "Invalid email address";
-                    },
-                  ),
-                  const SizedBox(
-                    height: AppDimens.padding_2x,
-                  ),
-                  InputField(
-                    hintText: "Password",
-                    onChanged: (val) {
-                      _name = val;
-                    },
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (email) {
-                      if (email != null && email != '') {
-                        return null;
-                      }
-                      return "Invalid email address";
-                    },
-                  ),
-                  const SizedBox(
-                    height: AppDimens.padding_2x,
-                  ),
-                  InputField(
-                    hintText: "Retype password",
-                    onChanged: (val) {
-                      _name = val;
-                    },
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (email) {
-                      if (email != null && email != '') {
-                        return null;
-                      }
-                      return "Invalid email address";
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: AppDimens.padding,
-          vertical: AppDimens.padding_3x,
-        ),
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ButtonStyle(
-            fixedSize:
-                MaterialStateProperty.all(const Size(double.infinity, 60.0)),
+                    Padding(
+                      padding: const EdgeInsets.all(AppDimens.padding_2x),
+                      child: Form(
+                        key: _editProfileKey,
+                        child: Column(
+                          children: [
+                            InputField(
+                              hintText: "Name",
+                              onChanged: (val) {
+                                _name = val;
+                                print(_name);
+                              },
+                              validator: (name) {
+                                if (name == '') {
+                                  return "Empty name";
+                                }
+                                return null;
+                              },
+                              textInputAction: TextInputAction.next,
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            const SizedBox(
+                              height: AppDimens.padding_2x,
+                            ),
+                            InputField(
+                              hintText: "Password",
+                              onChanged: (val) {
+                                _password1 = val;
+                                print("password1: $_password1");
+                              },
+                              validator: (password) {
+                                if (password == '')
+                                  return "The password is empty";
+                                else
+                                  return null;
+                              },
+                              obscureText: !bloc.visiblePassword,
+                              textInputAction: TextInputAction.done,
+                              keyboardType: TextInputType.visiblePassword,
+                            ),
+                            const SizedBox(
+                              height: AppDimens.padding_2x,
+                            ),
+                            // InputField(
+                            //   hintText: "Retype password",
+                            //   onChanged: (val) {
+                            //     _password2 = val ?? '';
+                            //   },
+                            //   textInputAction: TextInputAction.next,
+                            //   keyboardType: TextInputType.emailAddress,
+                            //   validator: (password) {
+                            //     if (_password1 == _password2) {
+                            //       return null;
+                            //     }
+                            //     return "The passwords are different";
+                            //   },
+                            //   obscureText: !bloc.visiblePassword,
+                            // ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
           ),
-          child: const Text("Save changes"),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppDimens.padding,
+            vertical: AppDimens.padding_3x,
+          ),
+          child: ElevatedButton(
+            onPressed: () async {
+              bool isValidated = _editProfileKey.currentState!.validate();
+              if (isValidated) {
+                try {
+                  print("here: $_name");
+                  await EditProfileBloc.changeUserNameAndPassword( _password1!,
+                    _name!,
+                  );
+                  print("here2");
+                  Navigator.pop(context);
+                } catch (e) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(e.toString())));
+                }
+              }
+            },
+            style: ButtonStyle(
+              fixedSize:
+                  MaterialStateProperty.all(const Size(double.infinity, 60.0)),
+            ),
+            child: const Text("Save changes"),
+          ),
         ),
       ),
     );
